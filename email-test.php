5f2bcce1a8091e79a2a4be018d2488d7183b34f1
Nabil Kashyap <n@nabilk.com>
2013-11-19 14:13:32 -0800

    test php tweaks

--- email-test.php
+++ email-test.php
@@ -1,17 +1,7 @@
 <?php
 session_start();
 
-if(count($_POST) > 0) {
-
-	$test = $_POST['teste'];
-
-	$handle = fopen('data/emails.txt', "a");
-	fwrite($handle, $test . "\n");
-	fclose($handle);
-
-}
-
-$html = "<form action='email-test.php' method='post'>";
+$html = "<form action='email.php' method='post'>";
 $html .= "<input type='hidden' name='teste' value='chunk'>";
 $html .= "<input type='submit'>";
 $html .= "</form>";
--- email.php
+++ email.php
@@ -1,18 +1,26 @@
 <?php
 
-$confirm = (count($_POST) > 0) ? 'yes' : 'nope';
+if(count($_POST) > 0) {
 
-$data = implode($_POST);
-$email_text = $_POST['plain'];
-$email_recepient = $_POST['headers']['To'];
-$email_sender = $_POST['headers']['From'];
-$email_subject = $_POST['headers']['Subject'];
+	if(count($_POST) > 1)
+		$data = implode($_POST);
+		$email_text = $_POST['plain'];
+		$email_recepient = $_POST['headers']['To'];
+		$email_sender = $_POST['headers']['From'];
+		$email_subject = $_POST['headers']['Subject'];
 
-$handle = fopen('data/emails.txt', "a");
-fwrite($handle, $confirm . " | " . $data . "\n");
-fclose($handle);
+		$handle = fopen('data/emails.txt', "a");
+		fwrite($handle, $confirm . " | " . $data . "\n");
+		fclose($handle);
+
+	else{
+		$test = $_POST['teste'];
+		$handle = fopen('data/emails.txt', "a");
+		fwrite($handle, $test . "\n");
+		fclose($handle);
+	}
+}
 ?>
-
 <!-- // session_start();
 
 // if(count($_POST) > 0){
