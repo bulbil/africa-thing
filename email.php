e32b8edb88858ccedf9a3d8213cd5517e3587050
Nabil Kashyap <n@nabilk.com>
2013-11-19 14:26:14 -0800

    test php tweaks

--- email.php
+++ email.php
@@ -1,48 +1,20 @@
 <?php
+  header("Content-type: text/plain");
 
-if(count($_POST) > 0) {
+  $to = $_POST['envelope']['to'];
+  $subject = $_POST['headers']['Subject'];
+  $plain = $_POST['plain'];
+  $html = $_POST['html'];
+  $reply = $_POST['reply_plain'];
 
-	if(count($_POST) > 1) {
-		$data = implode($_POST);
-		$email_text = $_POST['plain'];
-		$email_recepient = $_POST['headers']['To'];
-		$email_sender = $_POST['headers']['From'];
-		$email_subject = $_POST['headers']['Subject'];
-
-		$handle = fopen('data/emails.txt', "a");
-		fwrite($handle, $confirm . " | " . $data . "\n");
-		fclose($handle);
+  if (isset($to)){
+    header("HTTP/1.0 200 OK");
+	$handle = fopen('data/emails.txt', "a");
+	fwrite($handle, 'fire' . "|" . $subject . "|" . $plain . "\n");
+	fclose($handle);
 	}else{
-		$test = $_POST['teste'];
-		$handle = fopen('data/emails.txt', "a");
-		fwrite($handle, $test . "\n");
-		fclose($handle);
-	}
-
-}
+    header("HTTP/1.0 403 OK");
+    echo('user not allowed here');
+  }
+  exit;
 ?>
-<!-- // session_start();
-
-// if(count($_POST) > 0){
-
-// 	if (isset($to)){
-
-// 	header("Content-type: text/plain");
-
-// 	$to = $_POST['envelope']['to'];
-// 	$subject = $_POST['headers']['Subject'];
-// 	$plain = $_POST['plain'];
-// 	$html = $_POST['html'];
-// 	$reply = $_POST['reply_plain'];
-
-// 	$_SESSION[] = 'hi';
-// 	var_dump($_SESSION);
-// 	header("HTTP/1.0 200 OK");
-// 	header('email-test.php');
-// 	exit;
-// 	} else { 
-
-// 		echo('what'); 
-
-// 	}
-// } -->
