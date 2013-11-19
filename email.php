8f015d3a48dc35c32db0296fd693ecd5bda5a686
Nabil Kashyap <n@nabilk.com>
2013-11-19 01:35:38 -0800

    email.php

--- email.php
+++ email.php
@@ -1,2 +1,18 @@
 <?php
-var_dump($_POST);
+  header("Content-type: text/plain");
+
+  $to = $_POST['envelope']['to'];
+  $subject = $_POST['headers']['Subject'];
+  $plain = $_POST['plain'];
+  $html = $_POST['html'];
+  $reply = $_POST['reply_plain'];
+
+  if ($to == 'd1c1cc8ff02675728bf5@cloudmailin.net'){
+    header("HTTP/1.0 200 OK");
+    echo('success');
+    var_dump($_POST);
+  }else{
+    header("HTTP/1.0 403 OK");
+    echo('user not allowed here');
+  }
+  exit;
