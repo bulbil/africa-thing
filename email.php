645847f25a2a9f803844b2f414e02b399ef1e3fa
Nabil Kashyap <n@nabilk.com>
2013-11-19 14:35:34 -0800

    test php tweaks

--- email.php
+++ email.php
@@ -9,8 +9,8 @@
 
   if (isset($to)){
     header("HTTP/1.0 200 OK");
-	$handle = fopen('data/emails.txt', "a");
-	fwrite($handle, $subject . '\n');
+	$handle = fopen("data/emails.txt", "a");
+	fwrite($handle, $subject . "\n");
 	fclose($handle);
 	}else{
     header("HTTP/1.0 403 OK");
