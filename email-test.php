460522e6659f0f191ea08178b23f6f8caeba4fc2
Nabil Kashyap <n@nabilk.com>
2013-11-19 13:27:26 -0800

    email-test.php

--- email-test.php
+++ email-test.php
@@ -5,3 +5,10 @@ include 'email.php';
 
 var_dump($_SESSION);
 var_dump($_POST);
+
+$html = "<form action='email-test.php' method='post'>";
+$html .= "<input type='hidden' name='teste' value='chunk'>";
+$html .= "<input type='submit'>";
+$html .= "</form>";
+
+echo $html;
--- email.php
+++ email.php
@@ -1,21 +1,35 @@
 <?php
-session_start();
-header("Content-type: text/plain");
+$email_text = $_POST['message'];
+$email_sender = $_POST['from'];
+$email_subject = $_POST['email'];
 
-var_dump($_SESSION);
+$handle = fopen('emails.txt', "a");
+fwrite($handle, $email_subject . "|" . $email_text . "|" . $email_sender . "\n");
+fclose($handle);
+?>
 
-if(count($_POST) > 0){
-	$to = $_POST['envelope']['to'];
-	$subject = $_POST['headers']['Subject'];
-	$plain = $_POST['plain'];
-	$html = $_POST['html'];
-	$reply = $_POST['reply_plain'];
-}
+// session_start();
 
-if (isset($to)){
-$_SESSION['emails'][] = $_POST;
-var_dump($_SESSION);
-header("HTTP/1.0 200 OK");
-header('email-test.php');
-exit;
-} else {echo('what');}
+// if(count($_POST) > 0){
+
+// 	if (isset($to)){
+
+// 	header("Content-type: text/plain");
+
+// 	$to = $_POST['envelope']['to'];
+// 	$subject = $_POST['headers']['Subject'];
+// 	$plain = $_POST['plain'];
+// 	$html = $_POST['html'];
+// 	$reply = $_POST['reply_plain'];
+
+// 	$_SESSION[] = 'hi';
+// 	var_dump($_SESSION);
+// 	header("HTTP/1.0 200 OK");
+// 	header('email-test.php');
+// 	exit;
+// 	} else { 
+
+// 		echo('what'); 
+
+// 	}
+// }
