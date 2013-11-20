85890346b7be25bd2a40fc88e020c274d07d0a94
Nabil Kashyap <n@nabilk.com>
2013-11-19 21:29:58 -0800

    email.php now working

--- email.php
+++ email.php
@@ -1,20 +1,33 @@
 <?php
-  header("Content-type: text/plain");
+header("Content-type: text/plain");
 
-  $to = $_POST['envelope']['to'];
-  $subject = $_POST['headers']['Subject'];
-  $plain = $_POST['plain'];
-  $html = $_POST['html'];
-  $reply = $_POST['reply_plain'];
+$subject = $_POST['headers']['Subject'];
+$from = $_POST['headers']['From'];
+$to = $_POST['headers']['To'];
+$plain = $_POST['plain'];
 
-  if (isset($to)){
-    header("HTTP/1.0 200 OK");
-  	$handle = fopen("data/emails.txt", "a");
-  	fwrite($handle, $subject . "\n");
-  	fclose($handle);
-  }else{
-    header("HTTP/1.0 403 OK");
-    echo('user not allowed here');
+// $to = $_POST['envelope']['to'];
+// $html = $_POST['html'];
+// $reply = $_POST['reply_plain'];
+
+if (isset($to)){
+  header("HTTP/1.0 200 OK");
+	$handle = fopen("data/emails.txt", "a");
+	fwrite($handle, post_format($subject,'subject') . "," 
+                  . post_format($from) . ","
+                  . post_format($plain) . ",\n");
+	fclose($handle);
+}else{
+  header("HTTP/1.0 403 OK");
+  echo('user not allowed here');
+}
+exit;
+
+function post_format($str, $p = 'default') {
+  $str = trim($str);
+  switch($p){
+    case('default'): return $str;
+    case('from'): return $str;
+    case('subject'): return addslashes(strtolower($str));
   }
-  exit;
-?>
+}
