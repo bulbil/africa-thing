fefacdd020c097bda80b593c4f846890955f2dad
Nabil Kashyap <n@nabilk.com>
2013-11-25 00:50:31 -0800

    added clearinterval button keypress event

--- js/main.js
+++ js/main.js
@@ -10,16 +10,23 @@ var at = {
 
 	photos: [],
 
-	index: function(){
+	index: function(bool){
 
 		$.getJSON('json.php', function(d){
 
-			if(d.row_count !== d.row_check) at.updatePhotos(d,'change');
-			else console.log('update exit'); at.updatePhotos(d,'exit');
+			console.log('fire go' + bool);
+			if(bool === false) {
+
+				if(d.row_count !== d.row_check) at.updatePhotos(d,'change');
+				else console.log('update exit'); at.updatePhotos(d,'exit');
+
+			} else at.updatePhotos(d,'change');
+			
 			at.photos = [];
+
 		});
 		
-		},
+	},
 
 	updatePhotos: function(d,p) {
 
@@ -44,11 +51,8 @@ var at = {
 			case('exit'):
 				at.addPhotos([]);
 				break;
-			case('test'):
-				at.addPhotos([1,2,3]);
-				break;
 		}
-		},
+	},
 
 	addPhotos: function(data) {
 
@@ -57,8 +61,8 @@ var at = {
 
 		overlay.exit()
 			.transition()
-			.delay(function(d, i) { console.log('exit delay' + ++i); return 1000 * i; })
-			.duration(function(d, i) { console.log('exit dur' + ++i); return 500 * i; })
+			.delay(function(d, i) { return 1000 * ++i; })
+			.duration(function(d, i) { return 500 * ++i; })
 			.style("opacity", 0)			
 			.remove();
 
@@ -71,7 +75,6 @@ var at = {
 			.property("src", function(d,i) {
 					var url = 'http://farm' + d.farm + '.staticflickr.com/' 
 					+ d.server + '/' + d.id + '_' + d.secret + '.jpg'; 
-					console.log(url);
 					return url;})
 			// .style("background-image", function(d, i) {
 			// 	var url = 'url("http://farm' + d.farm + '.staticflickr.com/' 
@@ -81,37 +84,46 @@ var at = {
 				// return 'url("img/' + i + '.jpg")'; })
 			.style("opacity", 0)
 			.transition()
-			.delay(function(d,i) { console.log('enter delay ' + i); return 500 * i;})
-			.duration(function(d, i) { console.log('enter dur' + ++i); return 1000 * i;})
+			.delay(function(d,i) { return 500 * ++i;})
+			.duration(function(d, i) { return 1000 * ++i;})
 			.style("opacity", 1);
-		}
 	}
+}
 
-setInterval(at.index, 6000);
-
-var webcam = new Jscii({
+at.videoFeed = new Jscii({
 
 	container: document.getElementById("jscii-webrtc"),
 	el: document.getElementById("webrtc"),
 	webrtc: true
+
 });
+
+var bool = false;
 
 $(document).keydown(function(d) { 
 
 	var panelWidth = $(document).width() / $("div.panel").size();
 	console.log('keyCode ' + d.keyCode);
 	var xCurrent = $(document).scrollLeft();
-	
+
 	function skipAround(offset){
 		offset = (offset < 0) ? 0 : offset;
 	    $('html,body').animate( {scrollLeft: offset } , 'slow');
 	}
 
+
 	switch(d.keyCode) {
+
 		case(37): skipAround(xCurrent - panelWidth); break;
 		case(39): skipAround(xCurrent + panelWidth); break;
-		case(13): webcam.play(); break;
-		case(32): webcam.pause(); break;
+		case(13): at.videoFeed.play(); break;
+		case(32): at.videoFeed.pause(); break;
+		case(83):
+
+			if(!bool) clearInterval(at.indexTimer);
+			at.indexTimer = setInterval( function() { at.index(bool); }, 6000);			
+			bool = !bool;
+			break;
 	}
 
 	;})
