bfe5672a6b17db8ba4754953046fa5f88f63e590
Nabil Kashyap <n@nabilk.com>
2013-11-24 23:00:18 -0800

    changed flickr timing / added check for new emails

--- js/main.js
+++ js/main.js
@@ -12,58 +12,82 @@ var at = {
 
 	index: function(){
 
-		$.getJSON('http://www.nabilk.com/africa-thing/json.php', function(d){
+		$.getJSON('json.php', function(d){
 
-			if(d.row_count !== d.row_check) {
-				for (i in d.emails) {
-					var tag = d.emails[i].subject;
-					at.updatePhotos(tag);
-					}
-			}
-			});
-
-		at.photos = [];
+			if(d.row_count !== d.row_check) at.updatePhotos(d,'change');
+			else console.log('update exit'); at.updatePhotos(d,'exit');
+			at.photos = [];
+		});
+		
 		},
 
-	updatePhotos: function(tag) {
+	updatePhotos: function(d,p) {
 
-			var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4cb30dd217e4e782643dbeac982b4b50&tags=" + tag + "&sort=relevance&format=json&jsoncallback=?";
-			$.getJSON(url, function(d){
-				Object.defineProperty(d.photos.photo[0], "tag", {value : tag});
-				at.photos.push(d.photos.photo[0]);
-				at.addPhotos();
-			});
+		switch(p) {
+
+			case('change'):
+
+				var tags = $.map(d.emails, function(e) { return e.subject; })
+				
+				$.each(tags, function(i, f) {
+				
+					var url = "http://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=4cb30dd217e4e782643dbeac982b4b50&tags=" + f + "&sort=relevance&format=json&jsoncallback=?";
+
+					$.getJSON(url, function(g){
+						Object.defineProperty(g.photos.photo[0], "tag", {value : f});
+						at.photos.push(g.photos.photo[0]);
+						at.addPhotos(at.photos);
+					});
+				});
+				break;
+
+			case('exit'):
+				at.addPhotos([]);
+				break;
+			case('test'):
+				at.addPhotos([1,2,3]);
+				break;
+		}
 		},
 
-	addPhotos: function() {
+	addPhotos: function(data) {
 
 		var overlay = d3.select("div#overlay").selectAll("div.flickr")
-			.data(at.photos);
+			.data(data);
 
 		overlay.exit()
 			.transition()
-			.duration(function(d, i) { console.log('Exit'); return 500 * i;})
+			.delay(function(d, i) { console.log('exit delay' + ++i); return 1000 * i; })
+			.duration(function(d, i) { console.log('exit dur' + ++i); return 500 * i; })
 			.style("opacity", 0)			
 			.remove();
+
 
 		overlay.enter()
 			.append("div")
 			.attr("class", "flickr")
 			.style("height", function() { return $(document).height()/2 + 'px'; })
+			.append("img")
+			.property("src", function(d,i) {
+					var url = 'http://farm' + d.farm + '.staticflickr.com/' 
+					+ d.server + '/' + d.id + '_' + d.secret + '.jpg'; 
+					console.log(url);
+					return url;})
+			// .style("background-image", function(d, i) {
+			// 	var url = 'url("http://farm' + d.farm + '.staticflickr.com/' 
+			// 	+ d.server + '/' + d.id + '_' + d.secret + '.jpg")'; 
+			// 	console.log(url);
+			// 	return url;})
+				// return 'url("img/' + i + '.jpg")'; })
 			.style("opacity", 0)
 			.transition()
-			.duration(1000 * at.photos.length)
+			.delay(function(d,i) { console.log('enter delay ' + i); return 500 * i;})
+			.duration(function(d, i) { console.log('enter dur' + ++i); return 1000 * i;})
 			.style("opacity", 1);
-			// .transition()
-			// .style("opacity", 0)			
-			// .remove();
-
-		overlay.style("background-image", function(d) {
-			return 'url("http://farm' + d.farm + '.staticflickr.com/' + d.server + '/' + d.id + '_' + d.secret + '.jpg")'; });
 		}
 	}
 
-setInterval(at.index, 5000);
+setInterval(at.index, 6000);
 
 var webcam = new Jscii({
 
@@ -75,7 +99,7 @@ var webcam = new Jscii({
 $(document).keydown(function(d) { 
 
 	var panelWidth = $(document).width() / $("div.panel").size();
-	console.log('fire ' + d.keyCode);
+	console.log('keyCode ' + d.keyCode);
 	var xCurrent = $(document).scrollLeft();
 	
 	function skipAround(offset){
--- json.php
+++ json.php
@@ -3,19 +3,22 @@ session_start();
 $handle = fopen("data/emails.txt", "r");
 $columns = array('subject', 'from', 'plain');
 
+$i = 0;
 while($row = fgetcsv($handle)) {
 
-	if(count($row) > 1) $csv[] = array_combine($columns, $row);
+	if(count($row) > 1 && $i < 10) $csv[] = array_combine($columns, $row);
+	$i++;
 }
 
 fclose($handle);
 
-$row_check = (isset($_SESSION['row_check'])) ? $_SESSION['row_check'] : count($csv);
+$row_check = (isset($_SESSION['row_check'])) ? $_SESSION['row_check'] : $i;
+unset($_SESSION['row_check']);
 
-$data = array('row_count' => count($csv), 'row_check' => $row_check, 'emails' => $csv);
+$data = array('row_count' => $i, 'row_check' => $row_check, 'emails' => $csv);
 
 $json = json_encode($data);
 
 echo $json;
 
-$_SESSION['row_check'] = count($csv);
+$_SESSION['row_check'] = $i;
